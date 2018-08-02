66b987ea0d468702cf39a1c9b8db03b9faad6fac
Taewon Kim <rlaxodnjs199@gmail.com>
2018-08-02 13:47:51 -0400

    Update 3.LCwoRC.cpp

--- Leetcode/3.LCwoRC.cpp
+++ Leetcode/3.LCwoRC.cpp
@@ -34,12 +34,17 @@ This requires some types of storage to check previous appearance.
 Array vs Hashtable
 Both of them work here but I think array is more efficient here in terms of speed.
 Since string is composed of finite number of characters, we can declare fixed size array.
-Even though addition, deletion and find function of hashtable is O(1), it becomes worse when the number of entries is large enough that the hashtable needs to resize itself multiple times.
+Even though addition, deletion and find function of hashtable is O(1), 
+it becomes worse when the number of entries is large enough that the hashtable needs to resize itself multiple times.
 Therefore, since we can limit the size of entries, using array is a better idea.
 
-3. We initialize array as a size of "256" because it is a maximum ASCII number and we can react to all valid inputs. The way to initialize array with size 256 and give every index a value of "-1" is vector<int> array(256, -1); We need to initialize all the entries by "-1" so that we can easily check its previous appearance.
+3. We initialize array as a size of "256" because it is a maximum ASCII number and we can react to all valid inputs. 
+The way to initialize array with size 256 and give every index a value of "-1" is vector<int> array(256, -1); 
+We need to initialize all the entries by "-1" so that we can easily check its previous appearance.
 
 4. We don't need to declare "endindex" variable because "i" already has a role of indicating endindex of the current substring.
 
-**********5. [array[s[i]] >= start] because "start" should move only one direction toward end of the string. The reason is, even though a repeating happens for cetain character, it could possibly be overlapped by previous repeating characters. Therefore, we only need to consider "repeating character with biggest previous index".
+5.********** [array[s[i]] >= start] because "start" should move only one direction toward end of the string. 
+The reason is, even though a repeating happens for cetain character, it could possibly be overlapped by previous repeating characters. 
+Therefore, we only need to consider "repeating character with biggest previous index".
 */
