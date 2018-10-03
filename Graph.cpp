394d5e2375a915bd0eb9f55929e3e1e14accbe66
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2018-10-03 17:58:09 -0400

    Modift Graph

--- Graph.cpp
+++ Graph.cpp
@@ -2,32 +2,35 @@
 #include <vector>
 #include <utility>
 #include <string>
-#include <map>
+#include <unordered_map>
 struct vertex {
-    typedef std::pair<int, vertex*> ve;
-    std::vector<ve> adj; //cost of edge, destination vertex
-    std::string name;
-    vertex(std::string s) : name(s) {}
+  std::string start_vertex;
+  std::vector<std::pair<std::string, int>> edges;
+  vertex(std::string s): start_vertex(s) {}
 };
-
-class graph
-{
-public:
-    typedef std::map<std::string, vertex *> vmap;
-    vmap work;
-    void addvertex(const std::string&);
-    void addedge(const std::string& from, const std::string& to, double cost);
-};
-
-void graph::addvertex(const std::string &name)
-{
-    vmap::iterator itr = work.find(name);
-    if (itr == work.end())
-    {
-        vertex *v;
-        v = new vertex(name);
-        work[name] = v;
+class graph {
+  public:
+    std::unordered_map<std::string, vertex*> vertex_container;
+    void add_vertex(const std::string& start_vertex);
+    void add_edge(const std::string& start_vertex, const std::string& end_vertex, int cost);
+    
+    void add_vertex(const std::string& start_vertex) {
+      auto it = vertex_container.find(start_vertex);
+      if (it == vertex_container.end()) {
+        vertex_container[start_vertex] = new vertex(start_vertex);
         return;
+      }
+      std::cout << "Vertax with name " << start_vertex << " already exists" << std::endl;
     }
-    std::cout << "\nVertex already exists!";
-}
+    void add_edge(const std::string& start_vertex, const std::string& end_vertex, int cost) {
+      auto search_vertexes = vertex_container.find(start_vertex);
+      if (search_vertexes != vertex_container.end()) {
+        search_vertexes->second->edges.push_back(std::make_pair(end_vertex, cost));
+      }
+      std::cout << "Add Vertex " << start_vertex << " first!" << std::endl;
+    }
+};
+
+/*
+
+*/
