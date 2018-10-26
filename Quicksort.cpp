5384cfb2aa3e75c3866d9045a3ab05616466cc10
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2018-10-26 15:54:42 -0400

    Quicksort

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -4,7 +4,14 @@ Quick sort picks a pivot to compare with other elements.
 In this implementation, partitionIndex puts elements
 smaller than pivot to the left of the wall and the others
 to the right of the wall. Only pivot is in the right place
-after this function call. We repeat process again and again
+after this function call. we repeat this process again 
+and again until all the pivots are placed in the right place.
+dividing array to two parts help increasing algorithm speed by
+working like a heap data structure.
+2. Through this algorithm, I reminded that every recursive call
+algorithm has to have a certain condition statement to stop the
+call at a certain moment. Should not forget to give them
+a condition!
 */
 
 #include <iostream>
