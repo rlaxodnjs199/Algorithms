cb31e46097fe731154cab7e124d1ead2182e2dcb
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2019-03-12 22:43:00 -0400

    Fix#743

--- Leetcode/743.NetworkDelayTime.cpp
+++ Leetcode/743.NetworkDelayTime.cpp
@@ -4,11 +4,11 @@
 #include <algorithm>
 int networkdelay(std::vector<std::vector<int>>& times, int N, int K) {
   std::vector<int> delay(N, INT_MAX);
-  delay[K] = 0;
+  delay[K-1] = 0;
   while (--N) {
     for (auto& edge : times) {
-      if (delay[edge[0]] != INT_MAX) {
-        delay[edge[1]] = std::max(delay[edge[1]], delay[edge[0]] + delay[edge[2]]);
+      if (delay[edge[0]-1] != INT_MAX) {
+        delay[edge[1]-1] = std::min(delay[edge[1]-1], delay[edge[0]] + edge[2]);
       }
     }
   }
