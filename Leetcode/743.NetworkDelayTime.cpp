8fa0d39c66df04f77062922762beea2f68232d77
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2019-03-13 13:26:31 -0400

    Leetcode#743 Dijkstra

--- Leetcode/743.NetworkDelayTime.cpp
+++ Leetcode/743.NetworkDelayTime.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <climits>
 #include <algorithm>
-int networkdelay(std::vector<std::vector<int>>& times, int N, int K) {
+int networkdelay1(std::vector<std::vector<int>>& times, int N, int K) {
   std::vector<int> delay(N, INT_MAX);
   delay[K-1] = 0;
   while (--N) {
@@ -17,4 +17,37 @@ int networkdelay(std::vector<std::vector<int>>& times, int N, int K) {
     maxdelay = std::max(maxdelay, d);
   }
   return maxdelay == INT_MAX? -1 : maxdelay;
+}
+
+#include <queue>
+#include <algorithm>
+typedef std::pair<int, int> pii;
+int networkdelay2(std::vector<std::vector<int>>& times, int N, int K) {
+  std::vector<std::vector<pii>> graph(N + 1);
+  for (auto& edge : times) {
+    graph[edge[0]].push_back(std::make_pair(edge[1], edge[2]));
+  }
+  std::vector<int> dist(N + 1, INT_MAX);
+  dist[K] = 0;
+  std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
+  pq.push(std::make_pair(0, K));
+  std::vector<int> visited(N + 1, 0);
+  while (!pq.empty()) {
+    pii cur = pq.top();
+    pq.pop();
+    int src = cur.second;
+    if (!visited[src]) {
+      visited[src] = 1;
+      for (auto& edges : graph[src]) {
+        int dst = edges.first;
+        int c = edges.second;
+        if (dist[dst] > dist[src] + c) {
+          dist[dst] = dist[src] + c;
+          pq.push(std::make_pair(dist[dst], dst));
+        }
+      }
+    }
+    int result = *std::max_element(dist.begin(), dist.end());
+    return result == INT_MAX? -1 : result;
+  }
 }
